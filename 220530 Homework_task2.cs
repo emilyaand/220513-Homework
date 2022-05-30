3068b875bf30161647e551467fbd3b0745b110e9
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-05-30 18:33:24 +0400

    Add files via upload

---
+++ 220530 Homework_task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _220530_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 90, 60, 80 };
+            double overage = Calavg(grades);
+
+            if (overage > 60)
+                Console.WriteLine("Congrutilations, u are passed");
+            else
+                Console.WriteLine("Sorry, u are failed");
+        }
+
+        
+        static double Calavg(int[] arr)
+        {
+           int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum / arr.Length;
+        }
+    }
+}
