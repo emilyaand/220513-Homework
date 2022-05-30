4f929516a6fcecc92c8e191e0ce9fa4b64749bc5
EmiliyaAndamova <EmiliyaAndamova@WPIPCM>
2022-05-30 18:18:43 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _220530_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool a = Isprime(5);
+            Console.WriteLine(a);
+        }
+
+        
+        static bool Isprime(int number)
+        {
+            bool isPrime = true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if(number %1 == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
